c04904be5af7d4014a87ac3715e031a0a82ba64a
MintSW <mintnimal@gmail.com>
2021-03-29 01:15:49 +0700

    Add a new file "TabBar" and other environment

---
+++ ios-ui-guideline-app/TabBar.swift
@@ -0,0 +1,20 @@
+//
+//  TabBar.swift
+//  ios-ui-guideline-app
+//
+//  Created by Sansern Wuthirat on 3/29/21.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBar()
+    }
+}
