b0a394d5faed6f836760e03e24ddcfa085d7f182
MintSW <mintnimal@gmail.com>
2021-03-29 00:47:53 +0700

    Initial Commit

---
+++ ios-ui-guideline-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios-ui-guideline-app
+//
+//  Created by Sansern Wuthirat on 3/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios-ui-guideline-app/ios_ui_guideline_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_ui_guideline_appApp.swift
+//  ios-ui-guideline-app
+//
+//  Created by Sansern Wuthirat on 3/29/21.
+//
+
+import SwiftUI
+
+@main
+struct ios_ui_guideline_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
