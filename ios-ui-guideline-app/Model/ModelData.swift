a70a574282a9ac2579bcb7f62b6e374a620e76c4
MintSW <mintnimal@gmail.com>
2022-09-13 13:37:49 +0700

    Update files

--- ios-ui-guideline-app/Model/ModelData.swift
+++ ios-ui-guideline-app/Model/ModelData.swift
@@ -5,21 +5,21 @@
 //  Created by Sansern Wuthirat on 4/1/21.
 //
 
-import Foundation
-
-var systemColor: [colorData] = load("colorData.json")
-
-func load<T: Decodable>(_filename: String) -> T {
-    let data: Data
-    
-    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
-    else {
-        fatalError("Couldn't find \(filename) in main bundle.")
-    }
-    do {
-       data = try Data(contentsOf: file)
-    } catch {
-        fatalError()
-    }
-}
+//import Foundation
+//
+//var systemColor: [colorData] = load("colorData.json")
+//
+//func load<T: Decodable>(_filename: String) -> T {
+//    let data: Data
+//
+//    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+//    else {
+//        fatalError("Couldn't find \(filename) in main bundle.")
+//    }
+//    do {
+//       data = try Data(contentsOf: file)
+//    } catch {
+//        fatalError()
+//    }
+//}
 
--- ios-ui-guideline-app/View/ColorView.swift
+++ ios-ui-guideline-app/View/ColorView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ColorView: View {
     var body: some View {
         List {
-            ColorRow()
+           
         }
       
     }
